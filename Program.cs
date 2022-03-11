0261142c05edb2beca7278e531eabe451b5d4a30
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 20:54:29 +0530

    Ganesh Adding Greeting Message for Linked lis program

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LinkedListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to linked list program.");
+
+
+            //Console.WriteLine("Welcome to the Linked List Operations");
+            ////UseCase 1
+            //Operation obj = new Operation();
+            ////Insertion Operation
+            //obj.InsertLast(56);
+            //obj.InsertLast(30);
+            //obj.InsertLast(70);
+            ////Display Operation
+            //Console.WriteLine("Insertion at Last");
+            //obj.Display();
+            //Console.ReadLine();
+        }
+    }
+}
