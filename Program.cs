3d1747c65d92b1de83960441b8f6e41d08d1dd65
ganeshgaikwad56 <gaikwadganesh070297@gmail.com>
2022-03-11 21:01:00 +0530

    Ganesh Adding Program.cs for Linked list program

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ namespace LinkedListOperations
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to linked list program.");
+            //Console.WriteLine("Welcome to linked list program.");
 
 
-            //Console.WriteLine("Welcome to the Linked List Operations");
-            ////UseCase 1
-            //Operation obj = new Operation();
-            ////Insertion Operation
-            //obj.InsertLast(56);
-            //obj.InsertLast(30);
-            //obj.InsertLast(70);
-            ////Display Operation
-            //Console.WriteLine("Insertion at Last");
-            //obj.Display();
-            //Console.ReadLine();
+            Console.WriteLine("Welcome to the Linked List Operations");
+            //UseCase 1
+            Operation obj = new Operation();
+            //Insertion Operation
+            obj.InsertLast(56);
+            obj.InsertLast(30);
+            obj.InsertLast(70);
+            //Display Operation
+            Console.WriteLine("Insertion at Last");
+            obj.Display();
+            Console.ReadLine();
         }
     }
 }
